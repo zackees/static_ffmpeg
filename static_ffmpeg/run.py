5df5b32b9916df77942efb4269136a370a66773e
zackees <z@zackees.com>
2022-02-07 12:45:34 -0800

    now uses zip archive to reduce payload of static_ffmpeg

--- static_ffmpeg/run.py
+++ static_ffmpeg/run.py
@@ -6,20 +6,30 @@
 import os
 import stat
 import sys
+import zipfile
 
 SELF_DIR = os.path.dirname(__file__)
+ARCHIVE_ZIP = os.path.join(SELF_DIR, "archive.zip")
+VALID_PLATFORMS = ["win32", "linux", "darwin"]
+
+
+def get_platform_dir():
+    """Either get the executable or raise an error"""
+    if sys.platform not in VALID_PLATFORMS:
+        raise OSError(f"Please implement static_ffmpeg for {sys.platform}")
+    return os.path.join(SELF_DIR, sys.platform)
 
 
 def get_platform_executable_or_raise(fix_permissions=True):
     """Either get the executable or raise an error"""
+    exe_dir = get_platform_dir()
+    if not os.path.exists(exe_dir):
+        with zipfile.ZipFile(ARCHIVE_ZIP, "r") as zip_ref:
+            zip_ref.extractall(SELF_DIR)
+
+    ffmpeg_exe = os.path.join(exe_dir, "ffmpeg")
     if sys.platform == "win32":
-        ffmpeg_exe = os.path.join(SELF_DIR, "win32", "ffmpeg.exe")
-    elif sys.platform == "linux":
-        ffmpeg_exe = os.path.join(SELF_DIR, "linux", "ffmpeg")
-    elif sys.platform == "darwin":
-        ffmpeg_exe = os.path.join(SELF_DIR, "macos_x64", "ffmpeg")
-    else:
-        raise OSError(f"Please implement static_ffmpeg for {sys.platform}")
+        ffmpeg_exe = f"{ffmpeg_exe}.exe"
 
     if (
         fix_permissions
@@ -31,7 +41,7 @@ def get_platform_executable_or_raise(fix_permissions=True):
         read_bits = stat.S_IRUSR | stat.S_IRGRP | stat.S_IXGRP
         os.chmod(ffmpeg_exe, exe_bits | read_bits)
         assert os.access(ffmpeg_exe, os.X_OK), f"Could not execute {ffmpeg_exe}"
-        assert os.access(ffmpeg_exe, os.R_OK), f"Could not get read of {ffmpeg_exe}"
+        assert os.access(ffmpeg_exe, os.R_OK), f"Could not get read bits of {ffmpeg_exe}"
     return ffmpeg_exe
 
 
