3ad41e0fc680f373cea5046e209728e6a5c65835
zackees <z@zackees.com>
2025-03-15 19:11:05 -0700

    patch upload.sh

--- upload_package.sh
+++ upload_package.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 rm -rf build dist
-uv build
-echo "Uploading the package to PyPI via Twine…"
-uv add twine
-twine upload dist/* --verbose
+uv pip install wheel twine
+uv build --wheel
+uv run twine upload dist/* --verbose
 # echo Pushing git tags…
